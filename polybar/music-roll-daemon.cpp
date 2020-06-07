cb973e37956d6f21c79c4381a3bbd7817f060c50
rasarex <rasarex@o2.pl>
2020-06-07 14:01:23 +0200

    Fixed music script out of date behavior

--- polybar/music-roll-daemon.cpp
+++ polybar/music-roll-daemon.cpp
@@ -5,32 +5,43 @@
 #include <stdexcept>
 #include <string>
 #include <zmq.hpp>
-
+inline std::string trim(std::string orig)
+{
+  if (orig != "")
+    orig.pop_back();
+  return orig;
+}
 std::string roll(std::string preroll, size_t roll_by, size_t ret_size,
-                 std::string delimiter = "  ") {
+                 std::string delimiter = "  ")
+{
   preroll += delimiter;
   std::string retval;
   retval.resize(ret_size);
-  for (size_t i = 0; i < ret_size; ++i) {
+  for (size_t i = 0; i < ret_size; ++i)
+  {
     retval[i] = preroll[(i + roll_by) % preroll.size()];
   }
   return retval;
 }
 
-std::string exec(const char *cmd) {
+std::string exec(const char *cmd)
+{
   std::array<char, 128> buffer;
   std::string result;
   std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-  if (!pipe) {
+  if (!pipe)
+  {
     throw std::runtime_error("popen() failed!");
   }
-  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+  {
     result += buffer.data();
   }
   return result;
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
 
   zmq::context_t ctx{1};
   zmq::message_t mess;
@@ -39,26 +50,29 @@ int main(int argc, char const *argv[]) {
   std::string previous_message = "";
   int i = 0;
   sock.connect("tcp://localhost:8080");
-  while (true) {
+  while (true)
+  {
     update_message = exec("playerctl status");
-    if (update_message != "Playing\n") {
+    if (update_message != "Playing\n")
+    {
       sock.send(zmq::buffer(update_message), zmq::send_flags::dontwait);
       sock.recv(mess);
       continue;
     }
     // If status  is Playing this will return something as long as
     // there is metadata on a file
-    update_message = exec("playerctl metadata xesam:albumArtist 2> /dev/null");
-    update_message.pop_back();
+    update_message = trim(exec("playerctl metadata xesam:albumArtist 2> /dev/null"));
     update_message += " - ";
-    update_message += exec("playerctl metadata xesam:title 2> /dev/null");
-    update_message.pop_back();
+    update_message += trim(exec("playerctl metadata xesam:title 2> /dev/null"));
 
-    if (previous_message != update_message) {
+    if (previous_message != update_message)
+    {
       sock.send(zmq::buffer(roll(update_message, i++, 16) + '\n'),
                 zmq::send_flags::dontwait);
       sock.recv(mess);
-    } else {
+    }
+    else
+    {
       i = 0;
     }
     zmq_sleep(1);
